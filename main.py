81fb18b6c3db23a96db440dd5e9080fe9c8ffb16
Daniel du Preez <dandupreez@legiondev.co.za>
2024-02-13 11:22:47 +0200

    Store data in database

--- main.py
+++ main.py
@@ -3,9 +3,10 @@ import time
 import requests
 import selectorlib
 import smtplib, ssl
-import os
+import sqlite3
 
 URL = "https://programmer100.pythonanywhere.com/tours/"
+connection = sqlite3.connect('data.db')
 
 
 def scrape(url):
@@ -37,13 +38,22 @@ def send_email(message):
 
 
 def store(extracted):
-    with open("data.txt", "a") as file:
-        file.write(extracted + "\n")
+    row = extracted.split(",")
+    row = [item.strip() for item in row]
+    cursor = connection.cursor()
+    cursor.execute("INSERT INTO events VALUES(?,?,?)", row)
+    connection.commit()
 
 
 def read(extracted):
-    with open("data.txt", "r") as file:
-        return file.read()
+    row = extracted.split(",")
+    row = [item.strip() for item in row]
+    band, city, date = row
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM events WHERE band = ? AND city = ? AND date = ?", (band, city, date))
+    rows = cursor.fetchall()
+    print(rows)
+    return rows
 
 
 if __name__ == "__main__":
@@ -51,9 +61,10 @@ if __name__ == "__main__":
         scraped = scrape(URL)
         extracted = extract(scraped)
         print(extracted)
-        content = read(extracted)
+
         if extracted != "No upcoming tours":
-            if extracted not in content:
+            row = read(extracted)
+            if not row:
                 store(extracted)
                 send_email(message="Hey, new event was found!")
         time.sleep(2)
